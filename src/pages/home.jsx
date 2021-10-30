8510698483c5e69c89243a48770545bd824ee69e
Mr. Robot <harikrznan@gmail.com>
2021-10-30 18:44:50 +0530

    stories v1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Carousell from '../components/carousel'
 
 export default function Home() {
-  
+
     return (
         <div className="slider">
-           
+
             <section className="slide">
                 <Carousell />
             </section>
@@ -19,17 +19,38 @@ export default function Home() {
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus vero quo error quidem commodi repudiandae, animi facere repellendus blanditiis distinctio explicabo nulla aperiam eius, accusamus sint quos! Ullam ipsum dignissimos consequatur iure exercitationem sunt quae enim beatae! Officiis natus facere vel laudantium commodi, perferendis, quas tenetur voluptates corporis numquam quo autem assumenda doloremque totam dicta, sapiente recusandae minima nostrum voluptatibus quisquam similique! Sint voluptates itaque ex voluptatum harum consequatur nemo veniam illo tempore aliquam aut nostrum ullam nam, maiores excepturi animi ut adipisci, aliquid repellat? Autem vel eaque molestias nostrum officiis in, illum atque natus nisi voluptates repudiandae necessitatibus?
                         </p>
                     </div>
-                    
                 </div>
             </section>
             <section className="slide stories">
-                
+                <div class="grid h-screen  grid-cols-2 gap-4">
+                    <div className="w-full mx-20 pr-20">
+                        <div className=" mt-5 p-10 bg-green-200">
+                            <img className="mx-auto" src="imgs/stories1.png" alt="" />
+                            <p className="mx-12 text-justify">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eaque error vero labore iste iusto asperiores,
+                             nostrum culpa vel et repellendus possimus voluptates in unde fugit quibusdam voluptatem 
+                             
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full mx-20 pr-20">
+                        <div className=" mt-16 p-5 bg-green-200">
+                            <img className="mx-auto" src="imgs/stories2.png" alt="" />
+                            <p className="mx-12 text-justify">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eaque error vero labore iste iusto asperiores,
+                             nostrum culpa vel et repellendus possimus voluptates in unde fugit quibusdam voluptatem 
+                             
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </section>
             <section className="slide"></section>
 
             <section className="slide"></section>
 
-           
+
         </div>
     )
 }
