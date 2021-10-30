0197d69429e4517d9a95012cca4f2970d1ca9bf3
Mr. Robot <harikrznan@gmail.com>
2021-10-30 14:55:56 +0530

    carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react"; import "swiper/swiper-bundle.min.css";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
-
-export default function Carousell() {
+export default function Carousel() {
     return (
-        <div className="mx-auto">
-           <div class="main">
-    <div class="d1"></div>
-    <div class="d2"></div>
-    <div class="d3"></div>
-    <div class="d4"></div>
-</div>
+        <div>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false
+                }}
+               
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="h-screen w-screen flex justify-between">
+                        <div className="car1 w-screen">
+                            fklgn
+                        </div>
+                       
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="h-screen flex justify-between">
+                        <div className="">
+                            fklgn
+                        </div>
+                        <div>
 
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
         </div>
-    )
+    );
 }
