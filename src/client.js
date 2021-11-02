0b6f8cddf084c1c0963b0dde8df4eddf9b3e98a2
Mr. Robot <harikrznan@gmail.com>
2021-11-02 13:58:52 +0530

    env

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful'
 
 export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    space: 'ejrnifin7y02',
+    accessToken: 'yoEJwD_5lcw5vcpzz78OgEj68MmW54hsMaj9zLoCtLY',
 })
