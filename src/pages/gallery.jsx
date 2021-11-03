a78e3330b93ccf133dcfeaf22c6ef13c2a594775
Mr. Robot <harikrznan@gmail.com>
2021-11-03 19:22:11 +0530

    ghjmcg

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,48 +1,18 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
 import { photos } from "./photos";
 
-
 export default function Gallery1() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
-
-  return (
-    <div>
-      <div className="flex py-40 pt-56 items-center justify-center">
-        Gallery
-      </div>
-      <div style={{display: 'flex',justifyContent: 'center'}} className="">
-        <Gallery photos={photos} onClick={openLightbox} />
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <div>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map(x => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title
-                }))}
-              />
-              </div>
-            </Modal>
-          ) : null}
-        </ModalGateway>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex py-40 pt-56 items-center justify-center">
+                <div className="font-bold text-8xl">
+                    Gallery
+                </div>
+            </div>
+            <div className="max-w-7xl mx-auto">
+                <Gallery margin={5} photos={photos} />
+            </div>
+        </div>
+    )
 }
