4d67e7fa4e9ce78f01ebe24d4ac27117327bbcf5
am6074 <83806539+am6074@users.noreply.github.com>
2021-11-03 15:05:48 +0530

    typewriter

---
+++ typewriter/app.js
@@ -0,0 +1,38 @@
+var aText = new Array(
+    " A little help with a little smile gives meaning to human life.", 
+    "We make a living by what we get, but we live a life by what we give",
+    "Join us and see the need, not the cause"
+    );
+    var iSpeed = 72; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
