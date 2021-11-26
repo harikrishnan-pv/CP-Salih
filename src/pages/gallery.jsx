31418018ed3028021392839d583f08162b17c037
Ishwak Rahman <ishwak9526152121@gmail.com>
2021-11-26 18:53:07 +0530

    Videos
    Added 3 videos and changed some other stuff and oh !!! Amal is gay

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -10,6 +10,8 @@ import HoverVideoPlayer from "react-hover-video-player";
 import PausedOverlay from "./PausedOverlay";
 import LoadingOverlay from "./LoadingOverlay";
 import PausedOverlay2 from "./v2";
+import PausedOverlay3 from "./v3";
+import PausedOverlay4 from "./v4";
 
 
 export default function Gallery1() {
@@ -21,31 +23,75 @@ export default function Gallery1() {
                 </div>
             </div>
             <div className="max-w-7xl pb-20 mx-auto">
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     CFLTC Inaguration                </div>
                 <br />
 
                 <Gallery margin={15} photos={photos} />
-
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <br />
+                <br />
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     Mammootty Visiting CFLTC              </div>
                 <br />
                 <br />
 
-<div className="pl-3">
-    <HoverVideoPlayer
-        videoSrc="imgs\v2.mp4"
-        volume={0.5}
-        muted={false}
-        pausedOverlay={<PausedOverlay2 />}
-        loadingOverlay={<LoadingOverlay />}
-    />
-</div>
-<br />
-
                 <Gallery margin={15} photos={photos3} />
+                <br /> <br />
+                <div className="font-bold pl-4 pt-2 text-5xl">
+                    Wishes for CFLTC              </div>
+                <br />
 
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <div className = 'pl-1' >
+                    <HoverVideoPlayer
+                        videoSrc="imgs\v2.mp4"
+                        volume={0.5}
+                        muted={false}
+                        pausedOverlay={<PausedOverlay2 />}
+                        loadingOverlay={<LoadingOverlay />}
+                        style={{
+                            width: '98.5%'
+                        }}
+                    />
+                </div>
+
+                <div className="grid max-w-7xl pt-10 mx-auto lg:grid-cols-2 gap-9 pl-1" >
+                    <div>
+
+                        <div >
+                            <HoverVideoPlayer
+                                videoSrc="imgs\v3.mp4"
+                                volume={0.5}
+                                muted={false}
+                                pausedOverlay={<PausedOverlay3 />}
+                                loadingOverlay={<LoadingOverlay />}
+                                style={{
+                                    width: '98.5%'
+                                }}
+                            />
+                        </div>
+
+                    </div>
+
+                    <div>
+
+                        <div className = 'pt-1' >
+                            <HoverVideoPlayer
+                                videoSrc="imgs\v4.mp4"
+                                volume={0.5}
+                                muted={false}
+                                pausedOverlay={<PausedOverlay4 />}
+                                loadingOverlay={<LoadingOverlay />}
+                                style={{
+                                    width: '97.5%'
+                                }}
+                            />
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     Food Storage for CFLTC             </div>
                 <br />
 
@@ -62,19 +108,21 @@ export default function Gallery1() {
                         loadingOverlay={<LoadingOverlay />}
                     />
                 </div>
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <br /><br />
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     Food Distribution
                 </div>
                 <br />
 
                 <Gallery margin={15} photos={photos2} />
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <br /><br />
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     Free Vaccination Camp
                 </div>
                 <br />
                 <Gallery margin={15} photos={photos5} />
-
-                <div className="font-bold pl-4 pt-2 text-4xl">
+                <br /><br />
+                <div className="font-bold pl-4 pt-2 text-5xl">
                     Dream Home Project
                 </div>
                 <br />
