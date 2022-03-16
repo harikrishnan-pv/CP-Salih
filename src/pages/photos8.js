dfa5329a748168ece79b9519fa84c88cb3d2e3a2
Mr. Robot <harikrznan@gmail.com>
2022-03-16 20:44:34 +0530

    visual changes

--- src/pages/photos8.js
+++ src/pages/photos8.js
@@ -1,26 +1,26 @@
 export const photos8 = [
   {
     src: "imgs/z2.jpg",
-    width: 2.5,
+    width: 3,
     height: 1,
     class: "lg:hover:scale-110 z-50 transition transform",
   },
   {
     src: "imgs/z1.jpeg",
-    width: 2.5,
+    width: 1,
     height: 1,
     class: "lg:hover:scale-110 z-50 transition transform",
   },
   {
     src: "imgs/z3.jpg",
-    width: 3,
+    width: 4,
     height: 1,
     class: "kg:hover:scale-110 z-50 transition transform",
   },
   {
     src: "imgs/z5.jpg",
-    width: 16,
-    height: 9,
+    width: 3,
+    height: 2,
     class: "lg:hover:scale-110 z-50 transition transform",
   },
 ];
